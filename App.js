aabaac98dea5dd98a252948a6054284f4837cb66
Gamomen <harshashenoy63@Gmail.com>
2020-08-29 22:41:49 +0530

    React Native one

--- App.js
+++ App.js
@@ -6,6 +6,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is my first app.This is fucking crazy!</Text>
+      <Text>I like this. Its so beautiful and a smart way of developing.</Text>
+      <Text>Developing React Project</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
